20e30184b4d370e7afed8e227f8cb9185d5348f2
Nomokonov Ivan <nomokonov.vana@gmail.com>
2020-03-21 22:06:35 +0300

    BC-7 переключение страниц входа и регистрации

--- app/src/main/java/com/example/bookingcar/activities/enter/EnterActivity.java
+++ app/src/main/java/com/example/bookingcar/activities/enter/EnterActivity.java
@@ -1,4 +1,19 @@
 package com.example.bookingcar.activities.enter;
 
-public class EnterActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.bookingcar.R;
+
+public class EnterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter);
+
+        ViewPager pager = findViewById(R.id.pager);
+        pager.setAdapter(new EnterPageAdapter(this, getSupportFragmentManager()));
+    }
 }
--- app/src/main/java/com/example/bookingcar/activities/enter/EnterPageFragment.java
+++ app/src/main/java/com/example/bookingcar/activities/enter/EnterPageFragment.java
@@ -1,9 +1,12 @@
 package com.example.bookingcar.activities.enter;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -22,6 +25,10 @@ public class EnterPageFragment extends Fragment {
         return fragment;
     }
 
+    static String getTitle(Context context, int position) {
+        return position == 0 ? "Вход" : "Регистрация";
+    }
+
     public EnterPageFragment() {
     }
 
@@ -29,15 +36,23 @@ public class EnterPageFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         pageNumber = getArguments() != null ? getArguments().getInt("num") : 1;
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View result=inflater.inflate(R.layout.page_enter, container, false);
-//        TextView pageHeader=(TextView)result.findViewById(R.id.displayText);
-//        String header = String.format("Фрагмент %d", pageNumber+1);
-//        pageHeader.setText(header);
+        EditText name = result.findViewById(R.id.name);
+        EditText phone = result.findViewById(R.id.phone);
+        if(pageNumber == 0){
+            name.setVisibility(View.GONE);
+            phone.setVisibility(View.GONE);
+        } else {
+            name.setVisibility(View.VISIBLE);
+            phone.setVisibility(View.VISIBLE);
+        }
+
         return result;
     }
 }
